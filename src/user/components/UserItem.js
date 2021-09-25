1ceee5e3ba29c8cfee09555cad9eaf7fa3ea7eee
Soheil Nikroo <soheiln1234@gmail.com>
2021-09-25 01:26:57 -0700

    stylibg user item, list.

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,21 +1,26 @@
 import React from "react";
 
 import "./UserItem.css";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import { Link } from "react-router-dom";
+import Card from "../../shared/components/UIElements/Card";
 
 const UserItem = ({ id, image, name, placeCount }) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <h3>
-            {placeCount} {placeCount === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
