6df67e7341f615b8624ec281d86dfc02ff846a84
Soheil Nikroo <soheiln1234@gmail.com>
2021-09-25 01:15:21 -0700

    userItem added.

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,8 +2,22 @@ import React from "react";
 
 import "./UserItem.css";
 
-const UserItem = () => {
-  return <div></div>;
+const UserItem = ({ id, image, name, placeCount }) => {
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={image} alt={name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{name}</h2>
+          <h3>
+            {placeCount} {placeCount === 1 ? "Place" : "Places"}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default UserItem;
