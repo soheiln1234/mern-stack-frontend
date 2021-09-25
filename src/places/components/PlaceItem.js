1a3236223f256437c13a0951f1f04aaae98763a7
Soheil Nikroo <soheiln1234@gmail.com>
2021-09-25 10:18:11 -0700

    deletion warning modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -8,6 +8,7 @@ import Map from "../../shared/components/UIElements/Map";
 
 const PlaceItem = ({ image, title, description, address, id, cordinates }) => {
   const [showMap, setShowMap] = useState(false);
+  const [showConfimModal, setShowConfirmModal] = useState(false);
 
   const openMapHandler = () => {
     setShowMap(true);
@@ -15,6 +16,19 @@ const PlaceItem = ({ image, title, description, address, id, cordinates }) => {
 
   const closeMapHandler = () => {
     setShowMap(false);
+  };
+
+  const showDelteWarningHandler = () => {
+    setShowConfirmModal(true);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+  };
+
+  const confirmDeleteHandler = () => {
+    setShowConfirmModal(false);
+    console.log("Deleting..");
   };
 
   return (
@@ -31,6 +45,27 @@ const PlaceItem = ({ image, title, description, address, id, cordinates }) => {
           <Map center={cordinates} zoom={16} />
         </div>
       </Modal>
+      <Modal
+        show={showConfimModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <Fragment>
+            <Button onClick={cancelDeleteHandler} inverse>
+              CANCLE
+            </Button>
+            <Button onClick={confirmDeleteHandler} danger>
+              DELETE
+            </Button>
+          </Fragment>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this place? Please note that it
+          can't be undone there after.
+        </p>
+      </Modal>
       <li className="place-item">
         <Card className="place-item__content">
           <div className="place-item__image">
@@ -46,7 +81,9 @@ const PlaceItem = ({ image, title, description, address, id, cordinates }) => {
               VIEW ON MAP
             </Button>
             <Button to={`/places/${id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button onClick={showDelteWarningHandler} danger>
+              DELETE
+            </Button>
           </div>
         </Card>
       </li>
