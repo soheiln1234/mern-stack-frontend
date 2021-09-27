1f0d20a3e59f1ca419635a2abd9399b7929fd1fb
Soheil Nikroo <soheiln1234@gmail.com>
2021-09-27 05:55:40 -0700

    handeling cors error

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -29,8 +29,30 @@ const Auth = () => {
     },
   });
 
-  const authSubmitHandler = (event) => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
+
+    if (isLogin) {
+    } else {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
     auth.login();
   };
 
